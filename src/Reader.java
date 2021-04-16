23b871a2dd5d66c079494aafb831f700b7278ad5
alexwang05 <hpwzz20@gmail.com>
2021-04-16 10:57:39 -0700

    relative path

--- src/Reader.java
+++ src/Reader.java
@@ -2,12 +2,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class Reader {
-	
+
+	/**
+	 * Method to run every time program starts, creates a file if there is no file, and returns true if file was present
+	 * @return Boolean, if true -> file present from save, if false -> new file created
+	 */
 	public static Boolean checkFile() {
 		Boolean isFile;
 		
 		try {
-			File file = new File("data.txt");
+			File file = new File("Data/data.txt");
 			isFile = false;
 		}
 		
