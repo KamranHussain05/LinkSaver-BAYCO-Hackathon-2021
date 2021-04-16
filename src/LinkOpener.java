fb38a9a372acff08d2e44267c1b8fb2d4a9394d2
Connor Chow <legoman888@icloud.com>
2021-04-16 11:02:06 -0700

    Update LinkOpener.java

--- src/LinkOpener.java
+++ src/LinkOpener.java
@@ -1,4 +1,25 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class LinkOpener {
 
+	public void openLink(String s)
+	{
+		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) 
+		{
+			try {
+				Desktop.getDesktop().browse(new URI(s));
+			} catch (IOException | URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	
+	
 }
