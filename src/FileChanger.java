e9ba7bb81372535acbe4222a62fc1ed001b1c1a5
alexwang05 <hpwzz20@gmail.com>
2021-04-16 12:16:02 -0700

    Update FileChanger.java

--- src/FileChanger.java
+++ src/FileChanger.java
@@ -7,6 +7,17 @@ import java.util.ArrayList;
 
 public class FileChanger {
 
+	// test method - delete later
+	public static void main(String[] args) {
+		Data d = new Data(7);
+		try {
+			writeFile(d.returnStrings());
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Method to run every time program starts, creates a file if there is no file,
 	 * and returns true if file was present
@@ -21,8 +32,7 @@ public class FileChanger {
 			File file = new File("Data/data.txt");
 			Scanner fileReader = new Scanner(file);
 			isFile = true;
-		} 
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			isFile = false;
 		}
 
@@ -30,13 +40,28 @@ public class FileChanger {
 	}
 
 	/**
-	 * Called if checkFile returns true
+	 * Creates new file data.txt
+	 * Only call if checkFile() returns false
+	 * @return if file was successfully created
+	 */
+	public static void createFile() {
+		try {
+			File file = new File("Data/data.txt");
+			file.createNewFile();
+		} catch (IOException e) {
+			System.out.println("Error in createFile()");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Only call if checkFile() returns true or createNewFile() returns true;
 	 * 
 	 * @return String of file content
 	 */
 	public static String readFile() {
 		String data = ""; // initialize String
-		
+
 		try {
 			File file = new File("Data/data.txt");
 			Scanner fileReader = new Scanner(file);
@@ -51,26 +76,25 @@ public class FileChanger {
 			System.out.println("File is not found, stacktrace: ");
 			e.printStackTrace();
 		}
-		
+
 		return data;
 	}
-	
-	
+
 	/**
-	 * Called to write to existing .txt file
-	 * Only call if file is created
+	 * Called to write to existing .txt file Only call if file is created
+	 * 
 	 * @param info ArrayList of links and names
 	 * @throws IOException
 	 */
 	public static void writeFile(ArrayList<String> info) throws IOException {
 		FileWriter writer = new FileWriter("Data/data.txt");
-		
+
 		// loops through array
 		for (String s : info) {
 			// write each element to txt
 			writer.write(s + System.getProperty("line.seperator"));
 		}
-		
+
 		writer.close();
 	}
 
