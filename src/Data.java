6c06af99587c63139025609ee9a5112b5cf36543
Connor Chow <legoman888@icloud.com>
2021-04-17 11:19:57 -0700

    Added comments to Data

--- src/Data.java
+++ src/Data.java
@@ -1,9 +1,20 @@
 import java.util.ArrayList;
 
+/**
+ * Used to store data (strings) for the Linksaver, retrieve the data, as well as alter it
+ * 
+ * @author Alex Wang, Connor Chow, Kamran Hussain
+ * @version 4/17/2021
+ */
 public class Data {
 	
 	private static ArrayList<String> strings = new ArrayList<String>();
 
+	/**
+	 * Creates a data object
+	 * 
+	 * @param num number required for how many strings to store in data
+	 */
 	public Data(int num)
 	{
 		for(int i = 0; i < num; i++)
@@ -12,26 +23,44 @@ public class Data {
 		}
 	}
 	
+	/**
+	 * Replaces a specified string in the data object
+	 * 
+	 * @param num string index that will be replaced
+	 * @param s new string to replace old string at index num
+	 */
 	public void replaceStrings(int num, String s)
 	{
 		strings.remove(num);
 		strings.add(num,s);
 	}
 	
+	/**
+	 * Returns all strings in a Data object
+	 * @return arraylist of strings (number specified when initialized)
+	 */
 	public ArrayList<String> returnStrings()
 	{
 		return strings;
 	}
 	
+	/**
+	 * Returns string for one tile in homeGui
+	 * 
+	 * @param n number for index of line going to be retrieved in the Data object
+	 * @return requested string
+	 */
 	public String getFullString(int n) {
 		return strings.get(n);
 	
 	}
 	
 	/**
+	 * Gets the course name for one tile in homeGui
 	 * 
-	 * @param n
-	 * @return
+	 * @pre only zero or two ';' are used in the string
+	 * @param n number for index of line going to be retrieved in the Data object
+	 * @return course name at specified index
 	 */
 	public static String getCourseName(int n)
 	{
@@ -44,6 +73,14 @@ public class Data {
 		}
 		
 	}
+	
+	/**
+	 * Gets the course link for one tile in homeGui
+	 * 
+	 * @pre only zero or two ';' are used in the string
+	 * @param n number for index of line going to be retrieved in the Data object
+	 * @return course name at specified index
+	 */
 	public String getCourseLink(int n)
 	{
 		String s = strings.get(n);
@@ -55,6 +92,14 @@ public class Data {
 			return "";
 		}
 	}
+	
+	/**
+	 * Gets the meeting link for one tile in homeGui
+	 * 
+	 * @pre only zero or two ';' are used in the string
+	 * @param n number for index of line going to be retrieved in the Data object
+	 * @return course name at specified index
+	 */
 	public String getMeetingLink(int n)
 	{
 		String s = strings.get(n);
@@ -66,8 +111,9 @@ public class Data {
 		}
 	}
 	
-	
-	
+	/**
+	 * (Dev. test) used to print strings in console
+	 */
 	public void printStrings()
 	{
 		for(String s : strings)
