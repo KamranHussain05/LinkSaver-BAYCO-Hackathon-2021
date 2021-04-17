156c3cc556818b3af0aa48484f0681469afa94e1
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-04-16 22:32:10 -0700

    Update HomeGui.java

--- src/HomeGui.java
+++ src/HomeGui.java
@@ -10,7 +10,7 @@ public class HomeGui extends JFrame implements ActionListener {
 	private int courseCounter;
 	private Data d;
 	private JTextPane footer;
-	private static JTextPane title;
+	private JTextPane title;
 	private static JButton course1;
 	private static JButton course2;
 	private static JButton course3;
@@ -19,7 +19,7 @@ public class HomeGui extends JFrame implements ActionListener {
 	private static JButton course6;
 	private static JButton course7;
 	private static JButton course8;
-	private static JButton addCourse;
+	private  JButton addCourse;
 	
 	private static JButton editCourse1,editCourse2, editCourse3, editCourse4, editCourse5, editCourse6, editCourse7, editCourse8;
 	
@@ -51,10 +51,13 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 1
 	    course1 = new JButton();
 	    course1.setText(Data.getCourseName(0));
-	    course1.setEnabled(false);
+	    course1.addActionListener(this);
+	    this.add(course1);
 	    
 	    editCourse1 = new JButton();
 	    editCourse1.setText("Edit");
+	    editCourse1.addActionListener(this);
+	    this.add(editCourse1);
 	    course1.add(editCourse1);
 	    
 	    course1.setHorizontalAlignment(JTextField.CENTER);
@@ -65,10 +68,13 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 2
 	    course2 = new JButton();
 	    course2.setText(Data.getCourseName(1));
-	    course2.setEnabled(false);
+	    course2.addActionListener(this);
+	    this.add(course2);
 	    
 	    editCourse2 = new JButton();
 	    editCourse2.setText("Edit");
+	    editCourse2.addActionListener(this);
+	    this.add(editCourse2);
 	    course2.add(editCourse2);
 	    
 	    course2.setHorizontalAlignment(JTextField.CENTER);
@@ -79,24 +85,32 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 3
 	    course3 = new JButton();
 	    course3.setText(Data.getCourseName(2));
-	    course3.enable(false);
+	    course3.addActionListener(this);
+	    this.add(course3);
 	    
 	    editCourse3 = new JButton();
 	    editCourse3.setText("Edit");
+	    editCourse3.addActionListener(this);
+	    this.add(editCourse3);
 	    course3.add(editCourse3);
 	    
 	    course3.setHorizontalAlignment(JTextField.CENTER);
 	    course3.setBackground(new Color(230, 183, 97));
 	    course3.setFont(new Font("Antipasta Pro Regular", Font.PLAIN, 30));
+	    this.add(course3);
 	    panel.add(course3);
 	    
 	    //Tile 4
 	    course4 = new JButton();
 	    course4.setText(Data.getCourseName(3));
 	    course4.enable(false);
+	    course4.addActionListener(this);
+	    this.add(course4);
 	    
 	    editCourse4 = new JButton();
 	    editCourse4.setText("Edit");
+	    editCourse4.addActionListener(this);
+	    this.add(editCourse4);
 	    course4.add(editCourse4);
 	    
 	    course4.setHorizontalAlignment(JTextField.CENTER);
@@ -107,10 +121,13 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 5
 	    course5 = new JButton();
 	    course5.setText(Data.getCourseName(4));
-	    course5.enable(false);
+	    course5.addActionListener(this);
+	    this.add(course5);
 	    
 	    editCourse5 = new JButton();
 	    editCourse5.setText("Edit");
+	    editCourse5.addActionListener(this);
+	    this.add(editCourse5);
 	    course5.add(editCourse5);
 	    
 	    course5.setHorizontalAlignment(JTextField.CENTER);
@@ -121,10 +138,13 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 6
 	    course6 = new JButton();
 	    course6.setText(Data.getCourseName(5));
-	    course6.enable(false);
+	    course6.addActionListener(this);
+	    this.add(course6);
 	    
 	    editCourse6 = new JButton();
 	    editCourse6.setText("Edit");
+	    editCourse6.addActionListener(this);
+	    this.add(editCourse6);
 	    course6.add(editCourse6);
 	    
 	    course6.setHorizontalAlignment(JTextField.CENTER);
@@ -135,10 +155,13 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 7
 	    course7 = new JButton();
 	    course7.setText(Data.getCourseName(6));
-	    course7.enable(false);
+	    course7.addActionListener(this);
+	    this.add(course7);
 	    
 	    editCourse7 = new JButton();
 	    editCourse7.setText("Edit");
+	    editCourse7.addActionListener(this);
+	    this.add(editCourse7);
 	    course7.add(editCourse7);
 	    
 	    course7.setHorizontalAlignment(JTextField.CENTER);
@@ -149,10 +172,13 @@ public class HomeGui extends JFrame implements ActionListener {
 	    //Tile 8
 	    course8 = new JButton();
 	    course8.setText(Data.getCourseName(7));
-	    course8.enable(false);
+	    course8.addActionListener(this);
+	    this.add(course8);
 	    
 	    editCourse8 = new JButton();
 	    editCourse8.setText("Edit");
+	    editCourse8.addActionListener(this);
+	    this.add(editCourse8);
 	    course8.add(editCourse8);
 	    
 	    course8.setHorizontalAlignment(JTextField.CENTER);
@@ -195,8 +221,8 @@ public class HomeGui extends JFrame implements ActionListener {
 	    
 	    StyledDocument doc2 = footer.getStyledDocument();
 	    SimpleAttributeSet center2 = new SimpleAttributeSet();
-	    StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-	    doc.setParagraphAttributes(0, doc.getLength(), center, false);
+	    StyleConstants.setAlignment(center2, StyleConstants.ALIGN_CENTER);
+	    doc2.setParagraphAttributes(0, doc2.getLength(), center, false);
 	    
 	    Container c = getContentPane();
 	    c.add(panel, BorderLayout.CENTER);
@@ -265,41 +291,53 @@ public class HomeGui extends JFrame implements ActionListener {
 	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse2) {
-			System.out.println("edit course1 called");
+			System.out.println("edit course2 called");
 			AddCourse course = new AddCourse(d, 1);
 	    	course.setBounds(500, 500, 550, 350);
 	    	course.setBackground(new Color(211,211,211));
 	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse3) {
-			System.out.println("course3 called");
-			LinkOpener.openLink(d.getCourseLink(2));
-			LinkOpener.openLink(d.getMeetingLink(2));
+			System.out.println("edit course3 called");
+			AddCourse course = new AddCourse(d, 1);
+	    	course.setBounds(500, 500, 550, 350);
+	    	course.setBackground(new Color(211,211,211));
+	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse4) {
-			System.out.println("course4 called");
-			LinkOpener.openLink(d.getCourseLink(3));
-			LinkOpener.openLink(d.getMeetingLink(3));
+			System.out.println("edit course4 called");
+			AddCourse course = new AddCourse(d, 1);
+	    	course.setBounds(500, 500, 550, 350);
+	    	course.setBackground(new Color(211,211,211));
+	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse5) {
-			System.out.println("course5 called");
-			LinkOpener.openLink(d.getCourseLink(4));
-			LinkOpener.openLink(d.getMeetingLink(4));
+			System.out.println("edit course5 called");
+			AddCourse course = new AddCourse(d, 1);
+	    	course.setBounds(500, 500, 550, 350);
+	    	course.setBackground(new Color(211,211,211));
+	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse6) {
-			System.out.println("course6 called");
-			LinkOpener.openLink(d.getCourseLink(5));
-			LinkOpener.openLink(d.getMeetingLink(5));
+			System.out.println("edit course6 called");
+			AddCourse course = new AddCourse(d, 1);
+	    	course.setBounds(500, 500, 550, 350);
+	    	course.setBackground(new Color(211,211,211));
+	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse7) {
-			System.out.println("course7 called");
-			LinkOpener.openLink(d.getCourseLink(6));
-			LinkOpener.openLink(d.getMeetingLink(6));
+			System.out.println("edit course7 called");
+			AddCourse course = new AddCourse(d, 1);
+	    	course.setBounds(500, 500, 550, 350);
+	    	course.setBackground(new Color(211,211,211));
+	    	course.setVisible(true);
 		}
 		if(e.getSource() == editCourse8) {
-			System.out.println("course8 called");
-			LinkOpener.openLink(d.getCourseLink(7));
-			LinkOpener.openLink(d.getMeetingLink(7));
+			System.out.println("edit course8 called");
+			AddCourse course = new AddCourse(d, 1);
+	    	course.setBounds(500, 500, 550, 350);
+	    	course.setBackground(new Color(211,211,211));
+	    	course.setVisible(true);
 		}
 	}
 	
